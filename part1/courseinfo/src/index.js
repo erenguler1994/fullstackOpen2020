85badd34a1c6838d001cda8486d9430869286da0
Eren Güler <1994erenguler1994@users.noreply.github.com>
2020-08-15 02:25:10 +0300

    1.1 finished
    all the components updated

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -9,24 +9,15 @@ const Content = (props) => {
     return (
         <>
             <p>
-                {props.part1} {props.exercises1}
+                {props.part} {props.exercises}
             </p>
-            <p>
-                {props.part2} {props.exercises2}
-            </p>
-            <p>
-                {props.part3} {props.exercises3}
-            </p>
-            <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
         </>);
 };
 
 const Total = (props) => {
     return (
         <>
-            <Header course={props.course}/>
-            <Content part1={props.part1} exercises1={props.exercises1} part2={props.part2} exercises2={props.exercises2}
-                     part3={props.part3} exercises3={props.exercises3}/>
+            <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
         </>
     );
 };
@@ -41,9 +32,13 @@ const App = () => {
     const exercises3 = 14
 
     return (
-        <Total course={course} part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3}
-               exercises3={exercises3}>
-        </Total>
+        <>
+            <Header course={course}/>
+            <Content part={part1} exercises={exercises1}/>
+            <Content part={part2} exercises={exercises2}/>
+            <Content part={part3} exercises={exercises3}/>
+            <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+        </>
     )
 }
 
