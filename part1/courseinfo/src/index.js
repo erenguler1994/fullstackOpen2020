728b533a6bf0a3ce63dbf1f5058c1f550b4f948a
Eren Güler <1994erenguler1994@users.noreply.github.com>
2020-08-15 02:36:39 +0300

    1.2 completed
    Part component added

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,13 +5,22 @@ const Header = (props) => {
     return <h1>{props.course}</h1>;
 };
 
+const Part = (props) => {
+    return (
+        <p>
+            {props.part} {props.exercise}
+        </p>
+    );
+};
+
 const Content = (props) => {
     return (
         <>
-            <p>
-                {props.part} {props.exercises}
-            </p>
-        </>);
+            <Part part={props.part1} exercise={props.exercises1}/>
+            <Part part={props.part2} exercise={props.exercises2} />
+            <Part part={props.part3} exercise={props.exercises3} />
+        </>
+    );
 };
 
 const Total = (props) => {
@@ -34,10 +43,9 @@ const App = () => {
     return (
         <>
             <Header course={course}/>
-            <Content part={part1} exercises={exercises1}/>
-            <Content part={part2} exercises={exercises2}/>
-            <Content part={part3} exercises={exercises3}/>
-            <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+            <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3}
+                     exercises3={exercises3}/>
+            <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
         </>
     )
 }
